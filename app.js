2d87db750c3393f3eeced3012e841b564834d3b4
Emma-gin <emmanuelle.dufrenay@gmail.com>
2021-08-29 16:26:45 +0200

    fix : map js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ setInterval(function () {
 }, 5000);
 
 AOS.init();
+
+/* map change France to Bretagne */
+
 let dptFrance = document.getElementById("FR-BRE");
 let franceMap = document.getElementById("mapFrance");
 let bretagneMap = document.getElementById("mapBretagne");
@@ -17,6 +20,8 @@ dptFrance.addEventListener("mouseover", () => {
   bretagneMap.style.display = "block";
 });
 
+/* name dpt with mouse */
+
 let dptFinistere = document.getElementById("FR-29-1");
 let dptArmor = document.getElementById("FR-22-1");
 let dptMorbihan = document.getElementById("FR-56-1");
@@ -24,25 +29,14 @@ let textFinistere = document.getElementById("titreFinistere");
 let textArmor = document.getElementById("titrArmor");
 let textMorbihan = document.getElementById("titreMorbihan");
 
-dptFinistere.addEventListener("mouseover", () => {
-  textFinistere.style.display = "inline";
-});
-
-dptFinistere.addEventListener("mouseleave", () => {
-  textFinistere.style.display = "none";
-});
-
-dptArmor.addEventListener("mouseover", () => {
-  textArmor.style.display = "inline";
-});
-
-dptArmor.addEventListener("mouseleave", () => {
-  textArmor.style.display = "none";
-});
-dptMorbihan.addEventListener("mouseover", () => {
-  textMorbihan.style.display = "inline";
-});
-
-dptMorbihan.addEventListener("mouseleave", () => {
-  textMorbihan.style.display = "none";
-});
+let f = function (a, b) {
+  a.addEventListener("mouseover", () => {
+    b.style.display = "inline";
+  });
+  a.addEventListener("mouseleave", () => {
+    b.style.display = "none";
+  });
+};
+f(dptFinistere, textFinistere);
+f(dptArmor, textArmor);
+f(dptMorbihan, textMorbihan);
