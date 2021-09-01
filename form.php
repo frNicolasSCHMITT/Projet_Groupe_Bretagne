41b1b07f0e3180719b1c2a0192aa8b5276ec4e4d
Nico_SicariusFR <n.schmitt@codeur.online>
2021-09-01 13:41:07 +0200

    feat: form php

--- form.php
+++ form.php
@@ -1,3 +1,30 @@
 <?php
+    $errors = '';
+    $myemail = 'youremail@website.com';
+    if(
+        empty($_POST['name']) ||
+        empty($_POST['email']) ||
+        empty($_POST['message'])
+    ) {
+        $errors .= "\n Error: all fields are required";
+    }
 
+    $name = $_POST['name'];
+    $email_address = $_POST['email'];
+    $message = $_POST['message'];
+
+    if(empty($errors)) {
+        $to = $myemail;
+        $email_subjet = "Contact form submission: $name";
+        $email_body = "You have received a new message. " .
+            " Here are the details:\n Name: $name \n Email: $email_address \n Message: $message";
+
+        $header = "From: $myemail\n";
+        $header .= "Reply-To: $email_address";
+
+        mail($to, $email_subjet, $email_body, $header);
+
+        //redirect to the "thank you" page
+        header('location: contact_form_thank_you.html');
+    }
 ?>
